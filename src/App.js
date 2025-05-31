c1115a0ea5b62ac9dfabec6104a2de378f6fb4f1
nichoss26 <nicholassinclair056@gmail.com>
2025-05-31 20:18:01 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🔥 DOCKERIZED REACT APP 🔥</h1>
+      <p>Ini app React gw yang udah di-Dockerize pake Multi-Stage!</p>
+      <img 
+        src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDlqZ2V1dGx5Z2JtY2V6eWY2a2N1dHpjdjB6eWZ0bDZ0eGJmYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7TKsrfld1sntmJHi/giphy.gif" 
+        alt="Dancing Docker"
+        style={{ width: '300px' }}
+      />
+    </div>
+  );
+}
+
+export default App;
